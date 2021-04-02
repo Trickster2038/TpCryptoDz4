1f0ab4694dc5f84e01e3bed6e31a89ab75e56b61
Sergey <33185046+Trickster2038@users.noreply.github.com>
2021-04-03 01:34:10 +0300

    dz4 no readme

--- randomizer.py
+++ randomizer.py
@@ -1,4 +1,4 @@
-print("=== WELCOME TO RANDOMIZER ===")
+print("=== WELCOME TO TICKETS RANDOMIZER ===")
 # python randomizer.py -n 3 -f list.txt -p 33
 import sys
 import argparse
@@ -12,7 +12,7 @@ def createParser ():
     parser.add_argument ('-p', '--parameter')
     return parser
  
-def ticket_rand2(n, data, seed, forbidden):
+def ticket_rand(n, data, seed, forbidden):
     arg = data + ' ' + seed
     hash1 = hashlib.sha256(arg.encode('utf-8'))
     arg = hash1.hexdigest()
@@ -32,27 +32,23 @@ if __name__ == '__main__':
     parser = createParser()
     namespace = parser.parse_args(sys.argv[1:])
     print("\nn={} file={} param={}\n".format (namespace.numbilets,namespace.file,namespace.parameter) )
-    #print("file= {}".format (namespace.file) )
-    #print("param= {}\n\n".format (namespace.parameter) )
     f = open(str(namespace.file), encoding='utf-8')
     taken = []
     for line in f:
-    	print("{} : {}".format (line.strip(), 1+ticket_rand2(int(namespace.numbilets), line, namespace.parameter, taken)))
+    	print("{:35s}\t{}".format (line.strip()+':', 1+ticket_rand(int(namespace.numbilets), line, namespace.parameter, taken)))
     #print(taken)
     
     # benchmarks =====================================================================
 
-    print("\n===BENCHMARKS(deltas of distribution 10k records) ===\n")
+    print("\n=== BENCHMARKS(deltas of distribution 100k records) ===\n")
 
     a = [0]*int(namespace.numbilets)
     s_linear = [0]*int(namespace.numbilets)
     s_quadro = [0]*int(namespace.numbilets)
 
     for y in range(100000):
-    	i = ticket_rand2(int(namespace.numbilets), str(y), namespace.parameter, [])
-    	#print(i)
+    	i = ticket_rand(int(namespace.numbilets), str(y), namespace.parameter, [])
     	a[i] += 1
-    	#print(ticket_rand2(int(namespace.numbilets), str(y), namespace.parameter, []))
     for i in range(int(namespace.numbilets)):
     	print("delta[ticket{}] {:10.3f}%".format (i, 100*((a[i] / 100000.0)- (1.0/int(namespace.numbilets)))))
     	s_linear[i] = (100*((a[i] / 100000.0)- (1.0/int(namespace.numbilets)))) 
@@ -60,6 +56,6 @@ if __name__ == '__main__':
 
     avg_delta = s_linear[i] / int(namespace.numbilets)
     dispersion = s_quadro[i] / int(namespace.numbilets)
-    print("avg delta {:10.3f}%".format (avg_delta))
-    print("dispersion {:10.3f}%".format (dispersion))
+    print("avg delta:     {:10.3f}%".format (avg_delta))
+    print("dispersion:    {:10.3f}%".format (dispersion))
         
