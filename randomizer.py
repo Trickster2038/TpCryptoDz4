da25cd6394b6ce2d32a8a7c77c8f9bc0c5dbe146
Sergey <33185046+Trickster2038@users.noreply.github.com>
2021-04-03 13:36:35 +0300

    comments

--- randomizer.py
+++ randomizer.py
@@ -3,6 +3,7 @@ import argparse
 import sha3
 import hashlib
  
+# creates commandline params parser
 def createParser ():
     parser = argparse.ArgumentParser()
     parser.add_argument ('-n', '--numbilets')
@@ -10,7 +11,19 @@ def createParser ():
     parser.add_argument ('-p', '--parameter')
     return parser
  
+
 def ticket_rand(n, data, seed, forbidden):
+    """Returns unique ticket number in [0..n-1]
+    You need to increment it by 1 if you want get [1..n] 
+    Function generate random number using sha256 hash of input data + seed
+
+    Arguments:
+    n - max number of ticket -1
+    data - name of person, who gets ticket
+    seed - randomizer seed param
+    forbidden[] - array of already taken tickets
+
+    """
     arg = data + ' ' + seed
     hash1 = hashlib.sha256(arg.encode('utf-8'))
     arg = hash1.hexdigest()
@@ -21,8 +34,7 @@ def ticket_rand(n, data, seed, forbidden):
         arg = hash1.hexdigest()
         hash1_int = int(hash1.hexdigest(), 16)
         ticket = hash1_int % n
-        #print("tick : {}".format (data))
-
+        # you can see each iteration with 'print("tick : {}".format (data))'
     forbidden += [ticket]
     return ticket
  
@@ -34,25 +46,29 @@ if __name__ == '__main__':
     f = open(str(namespace.file), encoding='utf-8')
     taken = []
     for line in f:
-    	print("{:35s}\t{}".format (line.strip()+':', 1+ticket_rand(int(namespace.numbilets), line, namespace.parameter, taken)))
-    #print(taken)
+        # here we increment ticket number by 1 to get it in [1..n]
+        print("{:35s}\t{}".format (line.strip()+':', 1 + ticket_rand(int(namespace.numbilets), line, namespace.parameter, taken)))
     
     # benchmarks =====================================================================
 
-    print("\n=== BENCHMARKS(deltas of distribution 100k records) ===\n")
+    print("\n=== BENCHMARKS(deltas of distribution for 100k records) ===\n")
+    
+    tickets = [0]*int(namespace.numbilets) # array of tickets num repeats
 
-    a = [0]*int(namespace.numbilets)
     s_linear = 0
     s_quadro = 0
 
+    # in cycle we count number of repeats of each ticket number in [0..n-1]
     for y in range(100000):
-    	i = ticket_rand(int(namespace.numbilets), str(y), namespace.parameter, [])
-    	a[i] += 1
+        i = ticket_rand(int(namespace.numbilets), str(y), namespace.parameter, [])
+        tickets[i] += 1
+
+    # in cycle we collect statistic metrics of numbers distribution
     for i in range(int(namespace.numbilets)):
-    	delta = abs(100*((a[i] / 100000.0)- (1.0/int(namespace.numbilets))))
-    	print("delta[ticket{}] {:10.3f}%".format (i, delta))
-    	s_linear += delta 
-    	s_quadro += delta ** 2
+        delta = abs(100*((tickets[i] / 100000.0)- (1.0/int(namespace.numbilets))))
+        print("delta[ticket{}] {:10.3f}%".format (i, delta))
+        s_linear += delta 
+        s_quadro += delta ** 2
 
     avg_delta = s_linear / int(namespace.numbilets)
     dispersion = s_quadro / int(namespace.numbilets)
